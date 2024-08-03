0647d4346068ea61f8ba16c4bb52b8e32e7ff8d1
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-03 15:41:44 -0500

    testimonios-dev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Testimonio from "./Components/Testimonio";
+import Person from "../src/Data/testimonios.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Esto es lo que dicen nuestros alumnos:</h1>
+			<Testimonio
+				nombre={Person["testimony-01"].name}
+				pais={Person["testimony-01"].country}
+				imagen={Person["testimony-01"].image}
+				cargo={Person["testimony-01"].job}
+				empresa={Person["testimony-01"].company}
+				testimonio={Person["testimony-01"].comment}
+			/>
+			<Testimonio
+				nombre={Person["testimony-02"].name}
+				pais={Person["testimony-02"].country}
+				imagen={Person["testimony-02"].image}
+				cargo={Person["testimony-02"].job}
+				empresa={Person["testimony-02"].company}
+				testimonio={Person["testimony-02"].comment}
+			/>
+			<Testimonio
+				nombre={Person["testimony-03"].name}
+				pais={Person["testimony-03"].country}
+				imagen={Person["testimony-03"].image}
+				cargo={Person["testimony-03"].job}
+				empresa={Person["testimony-03"].company}
+				testimonio={Person["testimony-03"].comment}
+			/>
+		</div>
+	);
 }
 
 export default App;
